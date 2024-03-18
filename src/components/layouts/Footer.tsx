877561a026d91ca43b37920c43c7bcacf38ad19b
Swati <swati.c@exalogic.co>
2024-03-18 21:14:25 +0530

    login

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,27 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import home from "../../assets/img/home.svg";
+import homeActive from "../../assets/img/home-active.svg";
 import report from "../../assets/img/reports.svg";
+import reportActive from "../../assets/img/reports-active.svg";
 import profile from "../../assets/img/profile.svg";
-import { Link } from "react-router-dom";
+import profileActive from "../../assets/img/profile-active.svg";
 
 function Footer() {
+  const location = useLocation();
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    setIsActive(location.pathname.includes("/home"));
+  }, [location.pathname]);
+
   return (
     <>
-      <div className="footer mt-8 relative bg-[#0F1114] ">
-        <div className="footer-inner py-4 px-8 max-w-[375px] mx-auto">
-          <div className="grid grid-cols-12 gap-3 w-[100%]">
-            <div className="col-span-4">
-              <img src={home} alt="home"/>
-            </div>
-            <div className="col-span-4 mx-auto">
-              <img src={report} alt="home"/>
-            </div>
-            <div className="col-span-4 ms-auto">
-              <img src={profile} alt="home"/>
+      <div className="relative foot-main overflow-hidden">
+        <div className="footerBar h-[5px] w-[134px] absolute left-0 right-0 bottom-[0px] mb-2 z-[6] rounded-md bg-[#ffffff] mx-auto"></div>
+        <div className="footer bg-[#0F1114] z-1">
+          <div className="footer-inner py-4 px-8 max-w-[375px] mx-auto">
+            <div className="grid grid-cols-12 gap-3 w-[100%]">
+              <div className="col-span-4">
+                <Link to="/home">
+                  <img
+                    src={location.pathname === "/home" ? homeActive : home}
+                    alt="home"
+                    className={location.pathname === "/home" ? "active" : ""}
+                  />
+                </Link>
+              </div>
+              <div className="col-span-4 mx-auto">
+                <Link to="/reports">
+                  <img
+                    src={location.pathname === "/reports" ? reportActive : report}
+                    alt="report"
+                    className={location.pathname === "/" ? "active" : ""}
+                  />
+                </Link>
+              </div>
+              <div className="col-span-4 ms-auto">
+                <Link to="/profile">
+                  <img
+                    src={location.pathname === "/profile" ? profileActive : profile}
+                    alt="profile"
+                    className={location.pathname === "/profile" ? "active" : ""}
+                  />
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-        <div className="footerBar h-[5px] w-[134px] absolute left-0 right-0 bottom-[0px] mt-4 mb-2 z-6 rounded-md bg-[#ffffff] mx-auto"></div>
       </div>
     </>
   );
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Logo from "../../assets/img/3Dlogo.svg";
 import Google from "../../assets/img/google.png";
-import  check from "../../assets/img/checkbox.svg";
-import  user from "../../assets/img/user.svg";
-import  email from "../../assets/img/sms.svg";
-import  pass from "../../assets/img/lock.svg";
+import check from "../../assets/img/checkbox.svg";
+import user from "../../assets/img/user.svg";
+import email from "../../assets/img/sms.svg";
+import pass from "../../assets/img/lock.svg";
 import { Link } from "react-router-dom";
 import Footer from "../layouts/Footer";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
 
 function Login() {
   const [passwordVisible, setPasswordVisible] = useState(false);
-  const [inputType, setInputType] = useState('password');
+  const [inputType, setInputType] = useState("password");
 
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
-    setInputType(passwordVisible ? 'password' : 'text');
+    setInputType(passwordVisible ? "password" : "text");
   };
 
   return (
@@ -25,65 +25,129 @@ function Login() {
           <div className="login max-w-[375px] mx-auto px-5 md:px-0">
             <div className="grid grid-cols-12 gap-3 w-[100%]">
               <div className="col-span-12">
-                <img src={Logo} alt="Logo" className="text-center mx-auto mb-10" width={100} height={88}/>
-                <h1 className="text-white text-lg md:text-2xl lg:text-3xl font-semibold mb-1">Log In</h1>
-                <p className="text-[#9CA3AF] mb-5"> Create your account to get started </p>
+                <img
+                  src={Logo}
+                  alt="Logo"
+                  className="text-center mx-auto mb-10"
+                  width={100}
+                  height={88}
+                />
+                <h1 className="text-white text-lg md:text-2xl lg:text-3xl font-semibold mb-1">
+                  Log In
+                </h1>
+                <p className="text-[#9CA3AF] mb-5">
+                  {" "}
+                  Create your account to get started{" "}
+                </p>
                 <form className="">
                   <div className="mb-5 flex relative">
-                    <img src={user} alt="user" className="absolute z-2 left-[10px] top-[14px]"/>
-                    <input type="text" id="name" className=" bg-[#21252A] h-[48px] ps-[40px] border border-[#374151] text-[#9CA3AF] text-sm rounded-lg focus:ring-[#374151] focus:bg-[#21252A] focus:border-[#374151] block w-full p-2.5" placeholder="Your Name" required />
+                    <img
+                      src={user}
+                      alt="user"
+                      className="absolute z-2 left-[10px] top-[14px]"
+                    />
+                    <input
+                      type="text"
+                      id="name"
+                      className=" bg-[#21252A] h-[48px] ps-[40px] border border-[#374151] text-[#9CA3AF] text-sm rounded-lg focus:outline-none focus:border-[#374151] block w-full p-2.5"
+                      placeholder="Your Name"
+                      required
+                    />
                   </div>
                   <div className="mb-5 flex relative">
-                    <img src={email} alt="email" className="absolute z-2 left-[10px] top-[14px]"/>
-                    <input type="email" id="email" className="bg-[#21252A] h-[48px] ps-[40px] border border-[#374151] text-[#9CA3AF] text-sm rounded-lg focus:ring-[#374151] focus:bg-[#21252A] focus:border-[#374151] block w-full p-2.5" required placeholder="Email"/>
+                    <img
+                      src={email}
+                      alt="email"
+                      className="absolute z-2 left-[10px] top-[14px]"
+                    />
+                    <input
+                      type="email"
+                      id="email"
+                      className="bg-[#21252A] h-[48px] ps-[40px] border border-[#374151] text-[#9CA3AF] text-sm rounded-lg focus:outline-none focus:border-[#374151] block w-full p-2.5"
+                      required
+                      placeholder="Email"
+                    />
                   </div>
                   {/* <div className="mb-5 flex relative">
                     <img src={pass} alt="pass" className="absolute z-2 left-[10px] top-[14px]"/>
                     <FaRegEye className="absolute z-2 right-[10px] top-[15px] text-[#9CA3AF]"/>
                     <FaRegEyeSlash className="absolute z-2 right-[10px] top-[15px] text-[#9CA3AF]"/>
-                    <input type="password" id="password" className="bg-[#21252A] h-[48px] ps-[40px] border border-[#374151] text-[#9CA3AF] text-sm rounded-lg focus:ring-[#374151] focus:bg-[#21252A] focus:border-[#374151] block w-full p-2.5" required placeholder="Password"/>
+                    <input type="password" id="password" className="bg-[#21252A] h-[48px] ps-[40px] border border-[#374151] text-[#9CA3AF] text-sm rounded-lg focus:outline-none focus:border-[#374151] block w-full p-2.5" required placeholder="Password"/>
                   </div> */}
                   <div className="mb-5 flex relative">
-      <img src={pass} alt="pass" className="absolute z-2 left-[10px] top-[14px]"/>
-      {passwordVisible ? (
-        <FaRegEyeSlash
-          onClick={togglePasswordVisibility}
-          className="absolute z-2 right-[10px] top-[15px] text-[#9CA3AF] cursor-pointer"
-        />
-      ) : (
-        <FaRegEye
-          onClick={togglePasswordVisibility}
-          className="absolute z-2 right-[10px] top-[15px] text-[#9CA3AF] cursor-pointer"
-        />
-      )}
-      <input
-        type={inputType}
-        id="password"
-        className="bg-[#21252A] h-[48px] ps-[40px] border border-[#374151] text-[#9CA3AF] text-sm rounded-lg focus:ring-[#374151] focus:bg-[#21252A] focus:border-[#374151] block w-full p-2.5"
-        required
-        placeholder="Password"
-      />
-    </div>
+                    <img
+                      src={pass}
+                      alt="pass"
+                      className="absolute z-2 left-[10px] top-[14px]"
+                    />
+                    {passwordVisible ? (
+                      <FaRegEyeSlash
+                        onClick={togglePasswordVisibility}
+                        className="absolute z-2 right-[10px] top-[15px] text-[#9CA3AF] cursor-pointer"
+                      />
+                    ) : (
+                      <FaRegEye
+                        onClick={togglePasswordVisibility}
+                        className="absolute z-2 right-[10px] top-[15px] text-[#9CA3AF] cursor-pointer"
+                      />
+                    )}
+                    <input
+                      type={inputType}
+                      id="password"
+                      className="bg-[#21252A] h-[48px] ps-[40px] border border-[#374151] text-[#9CA3AF] text-sm rounded-lg focus:outline-none focus:border-[#374151] block w-full p-2.5"
+                      required
+                      placeholder="Password"
+                    />
+                  </div>
                   <div className="flex items-start mb-5">
                     <div className="flex items-center h-6 relative">
-                      <input  id="red-checkbox" type="checkbox" value="" className="custom-checkbox bg-gray-100 border-gray-300 rounded focus:ring-0 "/>
+                      <input
+                        id="red-checkbox"
+                        type="checkbox"
+                        value=""
+                        className="custom-checkbox bg-gray-100 border-gray-300 rounded focus:ring-0 "
+                      />
                       <div className="bg-[#FFFFFF] checked-check absolute">
-                        <img src={check} alt="checkbox"/>
+                        <img src={check} alt="checkbox" />
                       </div>
                     </div>
-                    <label className="ms-2 text-sm text-[#FFFFFF] h-6">I agree to terms & conditions</label>
+                    <label className="ms-2 text-sm text-[#FFFFFF] h-6">
+                      I agree to terms & conditions
+                    </label>
                   </div>
-                  <button type="submit" className="text-white h-[48px]  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center btn-primary mb-4">Create an account</button>
+                  <button
+                    type="submit"
+                    className="text-white h-[48px]  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center btn-primary mb-4"
+                  >
+                    Create an account
+                  </button>
                   <div className="sign_in mb-10">
-                    <p className="text-[#ffffff] text-sm text-center mb-5">Already have an account? <Link to="/home" className="text-[#04C1F6]">Sign in</Link> </p>
-                    <button type="submit" className="border-[#E5E5E5] flex items-center justify-center text-[#374151] h-[48px] bg-[#ffffff] focus:ring-0 focus:outline-none  font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center btn-primary-outline mb-4"><img src={Google} height="20" width="20" alt="google" className="me-2"/>Google</button>
-                  </div>                 
+                    <p className="text-[#ffffff] text-sm text-center mb-5">
+                      Already have an account?{" "}
+                      <Link to="/home" className="text-[#04C1F6]">
+                        Sign in
+                      </Link>{" "}
+                    </p>
+                    <button
+                      type="submit"
+                      className="border-[#E5E5E5] flex items-center justify-center text-[#374151] h-[48px] bg-[#ffffff] focus:ring-0 focus:outline-none  font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center btn-primary-outline mb-4"
+                    >
+                      <img
+                        src={Google}
+                        height="20"
+                        width="20"
+                        alt="google"
+                        className="me-2"
+                      />
+                      Google
+                    </button>
+                  </div>
                 </form>
               </div>
             </div>
           </div>
         </div>
-        <Footer/>   
+        <Footer />
       </section>
     </>
   );
